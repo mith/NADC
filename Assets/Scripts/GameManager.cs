e90995a1ebdc1a3ce18d6d798ee121c7135d439f
Simon Voordouw <simonvoordouw@gmail.com>
2016-06-11 17:43:06 +0200

    actually add gamemanager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class GameManager : NetworkManager
+{
+	public event Health.DeathDelegate EventDeath;
+
+	public override void OnServerAddPlayer (NetworkConnection conn, short playerControllerId)
+	{
+		var player = (GameObject)Instantiate (playerPrefab, GetStartPosition ().localPosition, Quaternion.identity);
+		player.GetComponent<PlayerScore> ().RegisterEvents (this);
+		player.GetComponent<Health> ().EventDeath += EventDeath;
+		NetworkServer.AddPlayerForConnection (conn, player, playerControllerId);
+	}
+}
