83563660b720798de912293d3d34f01c4e061ee6
Simon Voordouw <simonvoordouw@gmail.com>
2016-06-16 21:39:03 +0200

    stuff

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -13,7 +13,7 @@ public class Health : NetworkBehaviour
 
 	public event HealthChangeDelegate EventHealthChange;
 
-    bool Invulnerable = false;
+	bool Invulnerable = false;
 
 	public int maxHealth = 100;
 
@@ -29,31 +29,31 @@ public class Health : NetworkBehaviour
 	[Server]
 	public void TakeDamage (GameObject byPlayer, int amount)
 	{
-        if (Invulnerable)
-            return;
+		if (Invulnerable)
+			return;
         
-        NetworkInstanceId currentPlayerId = GetComponent<NetworkIdentity>().netId;
-        NetworkInstanceId byPlayerId = byPlayer.GetComponent<NetworkIdentity>().netId;
+		NetworkInstanceId currentPlayerId = GetComponent<NetworkIdentity> ().netId;
+		NetworkInstanceId byPlayerId = byPlayer.GetComponent<NetworkIdentity> ().netId;
 		int oldHealth = health;
 		health -= amount;
-        if (EventHealthChange != null) {
-            EventHealthChange(currentPlayerId, -amount, oldHealth, health);
-        }
+		if (EventHealthChange != null) {
+			EventHealthChange (currentPlayerId, -amount, oldHealth, health);
+		}
 		RpcBleedLight ();
-        Debug.Log("Player " + currentPlayerId + " took " + amount + " damage from player " + byPlayerId);
+		Debug.Log ("Player " + currentPlayerId + " took " + amount + " damage from player " + byPlayerId);
 		if (health <= 0) {
 			var spawn = NetworkManager.singleton.GetStartPosition ();
-            Respawn (spawn.position);
+			Respawn (spawn.position);
 
-            if (EventDeath != null) {
-                EventDeath(this.gameObject, byPlayer);
-            }
+			if (EventDeath != null) {
+				EventDeath (this.gameObject, byPlayer);
+			}
 	
-            if (EventHealthChange != null) {
-                EventHealthChange(currentPlayerId, maxHealth, 0, maxHealth);
-            }
+			if (EventHealthChange != null) {
+				EventHealthChange (currentPlayerId, maxHealth, 0, maxHealth);
+			}
 
-            Debug.Log("Player " + currentPlayerId + " was killed by " + byPlayerId);
+			Debug.Log ("Player " + currentPlayerId + " was killed by " + byPlayerId);
 		}
 	}
 
@@ -63,35 +63,36 @@ public class Health : NetworkBehaviour
 		transform.Find ("Blood").GetComponent<ParticleSystem> ().Emit (3);
 	}
 
-    [ClientRpc]
-    void RpcBleedHeavy ()
-    {
-        transform.Find ("Blood").GetComponent<ParticleSystem> ().Emit (6);
-    }
+	[ClientRpc]
+	void RpcBleedHeavy ()
+	{
+		transform.Find ("Blood").GetComponent<ParticleSystem> ().Emit (6);
+	}
 
 	void Respawn (Vector3 position)
 	{
-        StartCoroutine(RespawnTimer(position));
+		StartCoroutine (RespawnTimer (position));
 	}
 
-    IEnumerator RespawnTimer(Vector3 position)
-    {
-        GetComponent<PlayerController>().IsControlEnabled = false;
-        Invulnerable = true;
+	IEnumerator RespawnTimer (Vector3 position)
+	{
+		GetComponent<PlayerController> ().IsControlEnabled = false;
+		Invulnerable = true;
 
-        for(int x = 0; x < 20; x++) {
-            RpcBleedHeavy();
-            yield return new WaitForSeconds(0.2f);
-        }
+		for (int x = 0; x < 20; x++) {
+			RpcBleedHeavy ();
+			yield return new WaitForSeconds (0.2f);
+		}
 
-        GetComponent<PlayerController>().IsControlEnabled = true;
-        Invulnerable = false;
-        RpcSpawnAt(position);
-    }
+		GetComponent<PlayerController> ().IsControlEnabled = true;
+		Invulnerable = false;
+		RpcSpawnAt (position);
+		health = 100;
+	}
 
-    [ClientRpc]
-    void RpcSpawnAt(Vector3 position)
-    {
-        transform.position = position;
-    }
+	[ClientRpc]
+	void RpcSpawnAt (Vector3 position)
+	{
+		transform.position = position;
+	}
 }
